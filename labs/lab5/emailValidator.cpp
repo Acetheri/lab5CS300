28d47191ea231f526e4ad86e01b8a116e927b424
Upsettis <Alexeth@outlook.com>
2023-10-02 12:13:31 -0500

    fixed emailValidator

--- labs/lab5/emailValidator.cpp
+++ labs/lab5/emailValidator.cpp
@@ -7,7 +7,7 @@
 bool emailValidator(std::string s){
 
   
-  std::regex reg1("[1-0A-Za-z]{1-20}[.][\\[a-zA-Z0-9][@][a-z]{1-20}[.][a-z]{1-3}"); 
+  std::regex reg1("[0-9A-Za-z]{1,20}\\.[a-zA-Z0-9]+@[a-z]{1,20}\\.[a-z]{1,3}"); 
   return std::regex_match(s, reg1);
 
 
@@ -17,10 +17,10 @@ bool emailValidator(std::string s){
 
 int main(){
   if(emailValidator("joe.Mack@gmail.com")){
-    std::cout << "True";
+    std::cout << "True" << std::endl;
   }
   else{
-    std::cout << "false";
+    std::cout << "false"<< std::endl;
   }
   return 0;
 }
